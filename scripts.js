0fc41b8fb7c084ac129bf1d680913e7644e95d55
Tadeuvelloso <tadeuvellososilva@gmail.com>
2022-06-13 02:04:54 -0300

    ativação do seletor de pedido

--- scripts.js
+++ scripts.js
@@ -1,10 +1,33 @@
-function selecionador(prato) {
+function selecionadorprato(element) {
     
-    
-   let pratoselecionado = document.querySelector(".selecionado");
-   
+   const pratoselecionado = document.querySelector(".selecionado");
    if (pratoselecionado !== null){
-    pratoselecionado.classList.remove("selecionado");
+      pratoselecionado.classList.remove("selecionado");
    }
-   pratoselecionado.classList.add("selecionado");
+   element.classList.add("selecionado");
+}
+function ativariconeok(element) {
+
+   const ativaok = document.querySelector("prato .none");
+   if(ativaok !== null){
+      ativaok.classList.remove("none");
+   }
+   element.querySelector(".ok").classList.add("none");
+
+}
+function selecionadorbebida(element) {
+    
+   const bebidaselecionada = document.querySelector(".bebidas .selecionado");
+   if (bebidaselecionada !== null){
+      bebidaselecionada.classList.remove("selecionado"); 
+   }
+   element.classList.add("selecionado");
+}
+function selecionadorsobremesa(element) {
+    
+   const doceselecionado = document.querySelector(".sobremesas .selecionado");
+   if (doceselecionado !== null){
+      doceselecionado.classList.remove("selecionado");
+   }
+   element.classList.add("selecionado");
 }
