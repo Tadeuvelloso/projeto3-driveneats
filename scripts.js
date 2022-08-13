c0f7641a76d52878e1c10a48a6903d3bec315691
Tadeuvelloso <tadeuvellososilva@gmail.com>
2022-08-12 21:31:25 -0300

    implemantando scripts

--- scripts.js
+++ scripts.js
@@ -1,7 +1,51 @@
 
-function escolha (bolinha) {
-    const elemento = document.querySelector(".pratos .prato");
-    elemento.classList.add("selecionado");
+function escolhaPrato (elemento) {
     
+    const verde = document.querySelector('.pratos .selecionado');
 
+    if (verde !== null){
+        verde.classList.remove('selecionado');
+        cont --;
+    }
+     
+    elemento.classList.add('selecionado');
+    cont ++;
+    pedidoFinalizado ()
+}
+
+function escolhaBebida (elemento) {
+    
+    const verde = document.querySelector('.bebidas .selecionado');
+
+    if (verde !== null){
+        verde.classList.remove('selecionado');
+        cont --
+    }
+     
+    elemento.classList.add('selecionado');
+    cont ++;
+    pedidoFinalizado ()
+}
+
+function escolhaDoce (elemento) {
+    
+    const verde = document.querySelector('.doces .selecionado');
+
+    if (verde !== null){
+        verde.classList.remove('selecionado');
+        cont --;
+    }
+     
+    elemento.classList.add('selecionado');
+    cont ++
+    pedidoFinalizado ()
+}
+
+cont = 0;
+function pedidoFinalizado (){
+    if (cont === 3){
+    const final = document.querySelector(".finalizar");
+    final.innerHTML = "Finalizar Pedido"
+    final.classList.add("verdinho");
+    }
 }
